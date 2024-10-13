b190a87bbd636bef904fbb8d7c09147701d531e5
Nils Kjellman <nils.kjellman@polestar.com>
2024-10-13 23:04:06 +0200

    todos

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,24 +2,24 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+import * as $_listId_ from "./routes/[listId].tsx";
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Counter from "./islands/Counter.tsx";
+import * as $TodoListView from "./islands/TodoListView.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
+    "./routes/[listId].tsx": $_listId_,
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
   islands: {
     "./islands/Counter.tsx": $Counter,
+    "./islands/TodoListView.tsx": $TodoListView,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
